0c0cfd9dfc10426a7cfb4faea374a399e32ee31d
Elliot Weishaar <elliotweishaar27@gmail.com>
2023-09-16 08:38:19 -0500

    Updated bar and dwm theme

--- dwm/config.h
+++ dwm/config.h
@@ -13,19 +13,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "fira-code:size=12" };
 static const char dmenufont[]       = "fira-code:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#ffb86c";
+static const char col_gray3[]       = "#ff79c6";
+static const char col_gray4[]       = "#ffb86c";
+static const char col_cyan[]        = "#282a36";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	    /*               fg         bg         border   */
+	    [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+	        [SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "TERM", "WWW", "TUNE", "COMM", "GAME", /* "6", "7", "8", "9"*/ };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
