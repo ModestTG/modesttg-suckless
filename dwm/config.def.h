85619205c5fb38b746545929ff49cd365d3a9564
Elliot Weishaar <elliotweishaar27@gmail.com>
2023-09-21 13:14:44 -0500

    updated shortcuts

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -71,7 +71,9 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD ("flameshot gui") },
+	{ MODKEY,                       XK_b,      spawn,          SHCMD ("brave-browser") },
+	/* { MODKEY,                       XK_b,      togglebar,      {0} }, */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
